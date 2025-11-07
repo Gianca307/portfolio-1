37ca8dadcfb5fc2817f1053f483c5ed1d2a4939b
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-11-07 08:58:29 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://Gianca307.github.io/portfolio",
+  base: "/portfolio",
   plugins: [
     react(),
     tailwindcss()],
