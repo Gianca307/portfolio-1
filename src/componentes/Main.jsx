6cb753a210385d7cea5559e399aacca8789c8d41
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-11-07 07:31:49 -0300

    Crea Main component

---
+++ src/componentes/Main.jsx
@@ -0,0 +1,12 @@
+import Info from "./Info"
+
+ const Main = () => {
+
+    return (
+        <div className="flex items-center justify-center bg-orange-300 py-10 w-96 min-h-screen w-screen">
+            <Info />
+        </div>
+    )
+ }
+
+ export default Main
